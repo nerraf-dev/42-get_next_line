4248d95783da042fbbc5d2b6160fbbe0c97d708b
nerraf-dev <87069314+nerraf-dev@users.noreply.github.com>
2024-07-25 17:49:58 +0200

    chore: Update main.c to read from standard input or file

--- main.c
+++ main.c
@@ -1,19 +1,64 @@
-#include "./get_next_line/get_next_line.h"
+// #include "./get_next_line/get_next_line.h"
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	int		fd;
+// 	char	*line;
+
+// 	fd = open("test.txt", O_RDONLY);
+// 	line = get_next_line(fd);
+// 	while (line != NULL)
+// 	{
+// 		printf("%s", line);
+// 		free(line);
+// 		line = get_next_line(fd);
+// 	}
+// 	close(fd);
+// 	return (0);
+// }
+
+
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "./get_next_line/get_next_line.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	int		fd;
-	char	*line;
+    int		fd;
+    char	*line;
 
-	fd = open("test.txt", O_RDONLY);
-	line = get_next_line(fd);
-	while (line != NULL)
-	{
-		printf("%s", line);
-		free(line);
-		line = get_next_line(fd);
-	}
-	close(fd);
-	return (0);
+    if (argc == 1)
+    {
+        // Reading from standard input
+        fd = 0;
+    }
+    else if (argc == 2)
+    {
+        // Reading from a file
+        fd = open(argv[1], O_RDONLY);
+        if (fd < 0)
+        {
+            perror("Error opening file");
+            return (1);
+        }
+    }
+    else
+    {
+        fprintf(stderr, "Usage: %s [filename]\n", argv[0]);
+        return (1);
+    }
+
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("%s", line);
+        free(line);
+    }
+
+    if (fd != 0)
+        close(fd);
+
+    return (0);
 }
